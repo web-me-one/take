5186e3768f8192bd6e47a3115ffa4968c65edf23
web-me-one <harawathoko@gmail.com>
2020-10-01 02:50:12 +0200

    4

--- flaskr/aldb.py
+++ flaskr/aldb.py
@@ -3,7 +3,7 @@ from flask import Flask
 from   flask_sqlalchemy import SQLAlchemy
 
 app= Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL')#'sqlite:///test.db' #
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgres://yyilkblbrykehy:84cf0c8052645f4de0c2d7c64dce1ae3c781581f3358eae5f0efddd7c0de00c1@ec2-35-174-127-63.compute-1.amazonaws.com:5432/d8n77erpfco80f'#os.environ.get('DATABASE_URL')#'sqlite:///test.db' #
 db =SQLAlchemy(app)
 
 class  info(db.Model):
