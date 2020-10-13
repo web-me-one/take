2e7680a09807546a8c3ca1da48adefb3180e2611
web-me-one <harawathoko@gmail.com>
2020-10-13 04:01:35 +0200

    c

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -6,7 +6,7 @@ from   flask_sqlalchemy import SQLAlchemy
 
 from flask import ( Blueprint, flash, g, redirect, render_template, request, session, url_for,Flask )
 from werkzeug.security import check_password_hash, generate_password_hash
-from db import get_db
+#from db import get_db
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 app= Flask(__name__)
